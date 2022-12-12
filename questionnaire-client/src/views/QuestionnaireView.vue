e05027a14786e0a0f85c68594093791cb91a458f
kuba22z <your@email.com>
2022-12-12 14:43:50 +0100

    add questionnaireView

---
+++ questionnaire-client/src/views/QuestionnaireView.vue
@@ -0,0 +1,21 @@
+<template>
+<form>
+  <button>{{counter.count}}</button>
+</form>
+</template>
+
+<script>
+import {useCounterStore} from "@/stores/counter";
+
+export default {
+  name: "Questionnaire",
+  setup() {
+    const counter = useCounterStore();
+    return {counter}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
