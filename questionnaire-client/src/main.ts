71ad87e1da22a737e9ea922284a1232037f1510e
kuba22z <your@email.com>
2022-12-14 19:15:59 +0100

    install apollo and graphql for vue

--- questionnaire-client/src/main.ts
+++ questionnaire-client/src/main.ts
@@ -1,14 +1,35 @@
-import { createApp } from "vue";
+import { createApp, h, provide } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-const app = createApp(App);
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:4000/",
+});
 
+// Cache implementation
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  cache,
+  link: httpLink,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+});
 app.use(createPinia());
 app.use(router);
-
 app.mount("#app");
