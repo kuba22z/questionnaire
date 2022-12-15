e0da0f5d33d1cd60b1b482a8d43cce930218f95e
kuba22z <your@email.com>
2022-12-15 23:34:28 +0100

    move Question requests to separate file

---
+++ questionnaire-client/src/utils/requestQuestion.ts
@@ -0,0 +1,58 @@
+import type { UseQueryReturn } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+import { useQuery } from "@vue/apollo-composable";
+import type { DocumentNode } from "graphql/language";
+
+export class RequestQuestion {
+  nodeIdsContainer: number[];
+  response: UseQueryReturn<any, {}>;
+  QUESTION_NODE_BY_ID_QUERY: DocumentNode = gql`
+    query GetQuestionNodeById($nodeId: Int!) {
+      getQuestionNodeById(nodeId: $nodeId) {
+        recommendation
+        question {
+          text
+          answers {
+            nextNodeId
+            text
+          }
+        }
+      }
+    }
+  `;
+
+  constructor(variables: {}, nodeIdsContainer: number[]) {
+    this.nodeIdsContainer = nodeIdsContainer;
+    this.response = useQuery(this.QUESTION_NODE_BY_ID_QUERY, variables);
+  }
+
+  fetchNext = (nodeId: number | null) => {
+    if (nodeId !== null) {
+      this.nodeIdsContainer.push(nodeId);
+      this.fetchMore(nodeId);
+    } else {
+      alert("Error: Please select an option");
+    }
+  };
+
+  fetchMore = (nodeId: number) => {
+    this.response.fetchMore({
+      variables: { nodeId: nodeId },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        if (!fetchMoreResult) {
+          return previousResult;
+        }
+        return fetchMoreResult;
+      },
+    });
+  };
+
+  fetchPrev = () => {
+    if (this.nodeIdsContainer.length > 1) {
+      this.nodeIdsContainer.pop();
+      this.fetchMore(this.nodeIdsContainer[this.nodeIdsContainer.length - 1]);
+    } else {
+      alert("Error: There is no previous Question");
+    }
+  };
+}
--- questionnaire-client/src/views/HomeView.vue
+++ questionnaire-client/src/views/HomeView.vue
@@ -1,90 +1,88 @@
 <template>
-  <form id="formQuestion" @submit.prevent="fetchNextQuestion(pickedNextNodeId)">
-    <div v-if="error"> <GraphQLErrors :error="error"/></div>
-    <div v-if="loading">Loading...</div>
-    <div v-else-if="result">
-      <div v-if="result.getQuestionNodeById.question">
-          <Questionnaire :question="result.getQuestionNodeById.question" v-model:picked-next-node-id="pickedNextNodeId"/>
-      </div>
-      <div v-else>
-        Our recommendation: {{ result.getQuestionNodeById.recommendation }}
-      </div>
+  <div id="container">
+    <h1>Welcome to the smartphone-selector</h1>
+    <div id="form-container">
+      <form id="formQuestion" @submit.prevent="fetchNextQuestion(pickedNextNodeId)">
+        <div v-if="error">
+          <GraphQLErrors :error="error" />
+        </div>
+        <div v-if="loading">Loading...</div>
+        <div v-else-if="result">
+          <div v-if="result.getQuestionNodeById.question" id="questionnaire-container">
+            <Questionnaire :question="result.getQuestionNodeById.question"
+                           v-model:picked-next-node-id="pickedNextNodeId" />
+          </div>
+          <div v-else>
+            Our recommendation: {{ result.getQuestionNodeById.recommendation }}
+          </div>
 
-      <div>
-      <input type="submit" value="next Question">
-      <input type="button" id="back" @click="fetchPrevQuestion" value="previous Question" />
-      <br>
-      <input type="button" id="back" @click="fetchNextQuestion(1)" value="go to first Question" />
-      </div>
+          <div id="inputs">
+            <input type="submit" id="input" v-if="result.getQuestionNodeById.question"
+                   :disabled="disallowNextQuestion(result.getQuestionNodeById.question.answers,pickedNextNodeId)"
+                   value="next Question">
+            <input type="button" id="input"
+                   @click="fetchPrevQuestion" value="previous Question" />
+            <input type="button" id="input" @click="fetchNextQuestion(1)" value="go to first Question" />
+          </div>
+        </div>
+        <div v-else>Problems establishing a connection</div>
+      </form>
     </div>
-    <div v-else>Problems establishing a connection</div>
-
-  </form>
+  </div>
 </template>
 
 <script lang="ts">
-import gql from "graphql-tag";
-import { useQuery } from "@vue/apollo-composable";
 import GraphQLErrors from "@/components/GraphQLErrors.vue";
 import Questionnaire from "@/components/Questionnaire.vue";
 import { defineComponent } from "vue";
+import type { Answer } from "@/utils/types";
+import { RequestQuestion } from "@/utils/requestQuestion";
 
-const QUESTION_NODE_BY_ID_QUERY = gql`
-query GetQuestionNodeById($nodeId: Int!) {
-  getQuestionNodeById(nodeId: $nodeId) {
-    recommendation
-    question {
-        text
-      answers {
-        nextNodeId
-        text
-      }
-    }
-  }
-}
-`;
 
 export default defineComponent({
   name: "Home",
   components: { Questionnaire, GraphQLErrors },
   setup() {
-    const { result, loading, error, fetchMore } = useQuery(QUESTION_NODE_BY_ID_QUERY, { "nodeId": 1 });
-    const pickedNextNodeId = null;
-    const nodeIdsPath = [1];
 
-    const fetchNextQuestion = (nodeId: number) => {
-      nodeIdsPath.push(nodeId);
-      fetchMore({
-          variables: { "nodeId": nodeId },
-          updateQuery:
-            (previousResult, { fetchMoreResult }) => {
-              if (!fetchMoreResult) {
-                return previousResult;
-              }
-              return fetchMoreResult;
-            }
-        }
-      );
-    };
+    const requestQuestion= new RequestQuestion({"nodeId":1},[1])
 
-    const fetchPrevQuestion = () => {
-      nodeIdsPath.pop();
-      fetchNextQuestion(nodeIdsPath[nodeIdsPath.length - 1]);
-      nodeIdsPath.pop();
-    };
     return {
-      result,
-      loading,
-      error,
-      fetchNextQuestion,
-      fetchPrevQuestion,
-      pickedNextNodeId,
-      nodeIdsPath
+      result: requestQuestion.response.result,
+      loading: requestQuestion.response.loading,
+      error:requestQuestion.response.error,
+      fetchNextQuestion: requestQuestion.fetchNext,
+      fetchPrevQuestion: requestQuestion.fetchPrev,
+      nodeIdsContainer: requestQuestion.nodeIdsContainer
     };
   },
+  methods: {
+    disallowNextQuestion(answers: Answer[] | undefined, pickedNextNodeId: number): boolean {
+      return answers ? !answers.some(answer => answer.nextNodeId === pickedNextNodeId) : true;
+    }
+  },
+  data() {
+    return {
+      pickedNextNodeId: null
+    };
+  }
 });
 </script>
 
 <style scoped>
+#container {
+  border: 3px solid green;
+}
 
+#questionnaire-container {
+  margin: 20px;
+}
+
+input {
+  height: 40px;
+  margin-top: 20px;
+}
+
+#form-container {
+  margin: 50px
+}
 </style>
